6f6b0f667bb7bfbb7efc3022d66d2b03aa502d44
alaamahmoudgaad <alaa.mahmoud.gaad@gmail.com>
2026-04-19 01:58:01 +0200

    update

---
+++ Admin/dashboard.php
@@ -0,0 +1,18 @@
+
+
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php 
+include 'includes/temp/header.php';
+include 'includes/db/db.php';
+?>
+
+<h2>Hello From Dashboard</h2>
+
+<?php
+include 'includes/temp/footer.php';
+?>
