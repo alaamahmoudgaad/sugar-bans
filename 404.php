dfa03702987be3494e3d26275cdb23c673843b57
Basant Hamdy <basanthamdy656@gmail.com>
2026-04-29 22:46:00 +0300

    error page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+    header("HTTP/1.1 404 Not Found");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found - Sugar Bans</title>
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+<body class="error-body">
+
+    <h1 class="error-title">404 | Sorry!</h1>
+
+    <h4 class="error-line">The page you are looking for was not found or has been moved.</h4>
+
+    <a href="index.php" class="error-btn">Back to Home</a>
+
+</body>
+</html>
